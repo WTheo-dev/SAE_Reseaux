75b79d99ab34bbbecdeef8f3927987ad361b65e1
Theo <wazydrag.theo@gmail.com>
2023-11-20 20:47:33 +0100

    modifBDD

--- bdd_data.sql
+++ bdd_data.sql
@@ -1,6 +1,13 @@
-INSERT INTO `role` (`id_role`, `description`) VALUES
+-- Données pour la table `rôle`
+INSERT INTO `rôle` (`Id_Rôle`, `Description`) VALUES
 (1, 'Apprenti'),
 (2, 'Super-Admin'),
 (3, 'Educ Admin'),
 (4, 'Educ Simple'),
 (5, 'CIP');
+
+-- Ajout de la contrainte d'unicité pour le Super-Admin
+ALTER TABLE `rôle`
+ADD CONSTRAINT `unique_superadmin`
+UNIQUE (`description`);
+
