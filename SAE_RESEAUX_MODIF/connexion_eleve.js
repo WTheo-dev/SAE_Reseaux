866227b6f1e90d1277e3c62b575bd264388d4762
ZedroXxXx <noahbeugnet.pro@gmail.com>
2023-11-20 16:00:28 +0100

    Avancee page_postco_eleve

--- SAE_RESEAUX_MODIF/connexion_eleve.js
+++ SAE_RESEAUX_MODIF/connexion_eleve.js
@@ -4,9 +4,10 @@ document.addEventListener("DOMContentLoaded", function () {
     const clearButton = document.getElementById('clear-button-connexion-eleve');
     const validerButton = document.getElementById('valider-button-connexion-eleve');
 
-    const combinaisonValide = "1621"; // La combinaison correcte : vert-rouge-bleu-vert
+    const combinaisonValide = "1621"; // La combinaison correcte
 
     let enteredPattern = '';
+    let isCombinaisonCorrect = false;
 
     ronds.forEach((rond, index) => {
         rond.addEventListener('click', () => {
@@ -22,14 +23,13 @@ document.addEventListener("DOMContentLoaded", function () {
         passwordInput.value = '';
     });
 
-    validerButton.addEventListener('click', () => {
+    validerButton.addEventListener('click', (event) => {
         if (enteredPattern === combinaisonValide) {
+            isCombinaisonCorrect = true;
             alert("Combinaison correcte. Vous êtes connecté !");
-        } else {
-            alert("Combinaison incorrecte. Réessayez.");
-            enteredPattern = '';
-            passwordInput.value = '';
+        }
+        if (!isCombinaisonCorrect) {
+            event.preventDefault(); // Empêche la soumission du formulaire si la combinaison est incorrecte
         }
     });
 });
-     
