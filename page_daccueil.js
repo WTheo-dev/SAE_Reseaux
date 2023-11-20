c6b11ee3c3a253afe16fd63ab5e6455bdf4b6865
ZedroXxXx <noahbeugnet.pro@gmail.com>
2023-11-20 10:26:47 +0100

    PrenezCeluila

--- page_daccueil.js
+++
@@ -1,9 +0,0 @@
-let button = document.getElementById("js-button-tts");
-let content = document.getElementById("js-content-tts");
-
-button.addEventListener("click", function(){
-    let text = content.textContent;
-
-    let speech = new SpeechSynthesisUtterance(text);
-    speechSynthesis.speak(speech);
-});
