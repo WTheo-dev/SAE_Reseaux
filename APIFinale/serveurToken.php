af3ea4442c8f7abb9340deac117d90b2f34ff6db
Theo <wazydrag.theo@gmail.com>
2023-11-23 12:24:06 +0100

    modifUtilisateur

--- APIFinale/serveurToken.php
+++ APIFinale/serveurToken.php
@@ -15,12 +15,12 @@ switch ($methodeHTTP) {
 			$postedData = file_get_contents('php://input');
 			$data=json_decode($postedData, true);
 			if(empty($data['login']) AND empty($data['mdp'])){
-				$body = array("role" => "", "apprenti" => "", "exp" => (time()+600));
+				$body = array("role" => "", "utilisateur" => "", "exp" => (time()+600));
 				$RETURN_CODE = 201;
 			}else{
 				if(identification($data['login'], $data['mdp'])){
 					$RETURN_CODE = 201;
-					$body = array("role" => recuperation_role($data['login']), "apprenti" => $data['login'],"exp" => (time()+600));
+					$body = array("role" => recuperation_role($data['login']), "utilisateur" => $data['login'],"exp" => (time()+600));
 				}else{
 					$RETURN_CODE = 400;
 					$STATUS_MESSAGE = "Identifiant incorrect";
