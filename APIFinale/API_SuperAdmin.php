0cfc18186b0135370993301cf0b153e28dcd6c50
Theo <wazydrag.theo@gmail.com>
2023-11-26 21:27:48 +0100

    modifApi

--- APIFinale/API_SuperAdmin.php
+++ APIFinale/API_SuperAdmin.php
@@ -20,7 +20,31 @@ $data = json_decode($postedData, true);
 
 switch ($http_method) {
     case 'GET':
-
+        if ($role == 2) {
+            try {
+                // Supposons que vous ayez des fonctions listeApprenti et listeEducateur
+                $matchingData = [
+                    'listeApprenti' => listeApprenti($role),
+                    'listeEducateur' => listeEducateur($role),
+                ];
+    
+                $RETURN_CODE = 200;
+                $STATUS_MESSAGE = "Succes ! Les données autorisées pour votre rôle sont accessibles";
+            } catch (\Throwable $th) {
+                $RETURN_CODE = $th->getCode();
+                $STATUS_MESSAGE = $th->getMessage();
+                $matchingData = null;
+            } finally {
+                deliver_response($RETURN_CODE, $STATUS_MESSAGE, $matchingData);
+            }
+        } else {
+            // Gérer le cas où le rôle n'est pas égal à 2
+            $RETURN_CODE = 403;
+            $STATUS_MESSAGE = "Accès refusé. Vous n'avez pas les autorisations nécessaires.";
+            deliver_response($RETURN_CODE, $STATUS_MESSAGE, null);
+        }
+        break;
+    
     case 'POST':
 
     case 'DELETE':
