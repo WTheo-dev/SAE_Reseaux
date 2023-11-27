cd313c8de494762ac8dfc1c12472526b01f80ce9
Theo <wazydrag.theo@gmail.com>
2023-11-27 14:50:54 +0100

    addData

--- bdd_data.sql
+++ bdd_data.sql
@@ -1,4 +1,25 @@
--- Données pour la table `rôle`
+-- Insertion des données d'apprentis
+INSERT INTO `apprenti` (`nom`, `prenom`, `photo`, `id_utilisateur`) VALUES
+('Dupont', 'Jean', 'jean.jpg', 1),
+('Martin', 'Sophie', 'sophie.jpg', 2),
+('Dubois', 'Pierre', 'pierre.jpg', 3),
+('Lefevre', 'Marie', 'marie.jpg', 4),
+('Leroy', 'Luc', 'luc.jpg', 5),
+('Moreau', 'Claire', 'claire.jpg', 6),
+('Roux', 'Thomas', 'thomas.jpg', 7),
+('Fournier', 'Julie', 'julie.jpg', 8);
+
+-- Insertion des données pour la table `utilisateur`
+INSERT INTO `utilisateur` (`login`, `mdp`, `id_role`) VALUES
+('utilisateur1', 'motdepasse1', 1),
+('utilisateur2', 'motdepasse2', 2),
+('utilisateur3', 'motdepasse3', 3),
+('utilisateur4', 'motdepasse4', 4),
+('utilisateur5', 'motdepasse5', 4),
+('utilisateur6', 'motdepasse6', 5),
+('utilisateur7', 'motdepasse7', 1);
+
+-- Insertion des données pour la table `rôle`
 INSERT INTO `rôle` (`Id_Rôle`, `Description`) VALUES
 (1, 'Apprenti'),
 (2, 'Super-Admin'),
