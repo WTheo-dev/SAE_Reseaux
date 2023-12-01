e97e41adcd9baf26c81af24c027a2547c09c663b
Twisten <47831964+Twisten@users.noreply.github.com>
2023-12-01 15:05:01 +0100

    modif de plein de page

--- SAE_RESEAUX_MODIF/voir_commentaire_educ.js
+++ SAE_RESEAUX_MODIF/voir_commentaire_educ.js
@@ -3,6 +3,13 @@ window.onload = function() {
   loadComments();
 };
 
+function deconnecter() {
+  // Ajoutez ici le code pour déconnecter l'utilisateur, par exemple, en supprimant les cookies ou en effectuant une déconnexion côté serveur.
+
+  // Redirigez l'utilisateur vers la page d'accueil
+  window.location.href = "page_daccueil.html";
+}
+
 function ajouterCommentaire() {
   var commentaire = document.getElementById("comment").value.trim();
 
