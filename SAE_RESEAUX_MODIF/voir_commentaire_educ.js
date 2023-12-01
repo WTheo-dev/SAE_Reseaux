6495eb90c64dbf0ff29e8c313ad54ca8fa2f315f
Twisten <47831964+Twisten@users.noreply.github.com>
2023-12-01 12:20:14 +0100

    modif accent

--- SAE_RESEAUX_MODIF/voir_commentaire_educ.js
+++ SAE_RESEAUX_MODIF/voir_commentaire_educ.js
@@ -14,8 +14,8 @@ function ajouterCommentaire() {
       var cell2 = row.insertCell(1);
       var cell3 = row.insertCell(2);
       var cell4 = row.insertCell(3);
-      cell1.innerHTML = "Nouveau";
-      cell2.innerHTML = "Commentaire";
+      cell1.innerHTML = "Jean";
+      cell2.innerHTML = "Dubois";
       cell3.innerHTML = commentaire;
 
       // Add a "Supprimer" button only for the new row
