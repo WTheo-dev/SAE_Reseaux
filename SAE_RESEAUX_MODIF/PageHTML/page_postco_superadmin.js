8c2ae3c47a5e12e0b92fda7ae1ba8a12a666fc6c
ZedroXxXx <noahbeugnet.pro@gmail.com>
2023-12-20 17:57:14 +0100

    bouton_GO fonctionnel page_postco_superadmin

--- SAE_RESEAUX_MODIF/PageHTML/page_postco_superadmin.js
+++ SAE_RESEAUX_MODIF/PageHTML/page_postco_superadmin.js
@@ -33,7 +33,48 @@ function showSection() {
       case "creereleves":
         window.location.href = "creer_compte_eleve.html";
         break;
-      case
+      case "modifiereleves":
+        window.location.href = "modifier_compte_eleve.html";
+        break;
+      case "supprimereleves":
+          window.location.href = "supprimer_eleve.html";
+        break;
+        case "creercours":
+        window.location.href = "creer_cours.html";
+        break;
+      case "modifiercours":
+        window.location.href = "modifier_cours.html";
+        break;
+      case "supprimercours":
+          window.location.href = "supprimer_cours.html";
+        break;
+        case "creereducateurs":
+        window.location.href = "creer_educateur.html";
+        break;
+      case "modifiereducateurs":
+        window.location.href = "modifier_educateur.html";
+        break;
+      case "supprimereducateurs":
+          window.location.href = "supprimer_educateur.html";
+        break;
+           case "creerformations":
+        window.location.href = "creer_formation.html";
+        break;
+      case "modifierformations":
+        window.location.href = "modifier_formation.html";
+        break;
+      case "supprimerformations":
+          window.location.href = "supprimer_formation.html";
+        break;
+        case "creeradministrateur":
+          window.location.href = "creer_compte_superadmin.html";
+          break;
+        case "modifieradministrateur":
+          window.location.href = "modifier_superadmin.html";
+          break;
+        case "supprimeradministrateur":
+            window.location.href = "supprimer_superadmin.html";
+          break;
     }
 
     
