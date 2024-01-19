e938b1b5a2e7478976f32d5f36adf093b1ae42fe
Xafia <t.lac-64@laposte.net>
2024-01-19 09:52:40 +0100

    Modif pages

--- SAE_RESEAUX_MODIF/PageHTML/voir_commentaire_educ.php
+++ SAE_RESEAUX_MODIF/PageHTML/voir_commentaire_educ.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Page connecté éducateur technique</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Page éducateur technique</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" charset="UTF-8">
     <link rel="stylesheet" href="style.css">
     <header class="header-voir_com_educ">
       <div class="logo">
@@ -12,6 +12,13 @@
   </head>
   <body class="body-voircom">
 
+  <?PHP
+
+  $sql = "SELECT commentaire_texte FROM laisser_trace";
+
+ 
+  ?>
+
     <div class="w3-container-voircom">
 
       <h2 class="h2-voircom">Commentaires de la fiche</h2>
@@ -19,7 +26,7 @@
       <label class="label-voircom" for="comment">Ajouter un commentaire:</label>
       <textarea class="textarea-voircom" id="comment-voircom" name="comment-voircom" rows="4" cols="50"></textarea><br>
       
-      <button class="button-voircom" onclick="ajouterCommentaire()">Ajouter commentaire</button>
+      <button class="button-voircom" onclick="ajouterCommentaire()">Ajouter un commentaire</button>
       
       <div class="w3-responsive-voircom">
         <table id="commentTablevoircom" class="w3-table-all-voircom">
