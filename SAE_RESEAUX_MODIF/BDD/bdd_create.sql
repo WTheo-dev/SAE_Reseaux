040a4423acb0495ff226daa5a6dc96f96d9fd822
Twisten <47831964+Twisten@users.noreply.github.com>
2024-03-18 09:46:38 +0100

    changement user

--- SAE_RESEAUX_MODIF/BDD/bdd_create.sql
+++ SAE_RESEAUX_MODIF/BDD/bdd_create.sql
@@ -1,6 +1,6 @@
-CREATE USER IF NOT EXISTS 'utilisateur'@'localhost' IDENTIFIED BY 'password';
+CREATE USER IF NOT EXISTS 'maxlamenace'@'localhost' IDENTIFIED BY 'hamza';
 CREATE DATABASE IF NOT EXISTS apeaj
 CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-GRANT ALL PRIVILEGES ON apeaj .* TO 'utilisateur'@'localhost';
+GRANT ALL PRIVILEGES ON apeaj .* TO 'maxlamenace'@'localhost';
 FLUSH PRIVILEGES;
 
