79110b7535fe272bd066462784560873b85d6443
_N3m0 <n3m0o7@hotmail.com>
2024-03-19 15:10:14 +0100

    rm bdd useless user

--- SAE_RESEAUX_MODIF/BDD/bdd_create.sql
+++ SAE_RESEAUX_MODIF/BDD/bdd_create.sql
@@ -1,5 +1,3 @@
-CREATE USER IF NOT EXISTS 'root'@'localhost' IDENTIFIED BY 'root';
-CREATE USER IF NOT EXISTS 'utilisateur'@'localhost' IDENTIFIED BY 'password';
 CREATE USER IF NOT EXISTS 'maxlamenace'@'localhost' IDENTIFIED BY 'hamza';
 CREATE DATABASE IF NOT EXISTS apeaj
 CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
