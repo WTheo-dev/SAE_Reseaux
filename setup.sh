ce43c013d9ccb2c8f19e624f15a9d6a0f96f9865
_N3m0 <n3m0o7@hotmail.com>
2024-03-22 14:37:11 +0100

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,4 +8,8 @@ cd SAE_Reseaux && git checkout integration-de-fou-zinzin && cd ..
 
 cp ./SAE_Reseaux/docker-compose.yml . && docker-compose up -d
 
+docker exec sae chmod +x /opt/lampp/htdocs/init-bdd.sh
+
+sleep 10
+
 docker exec sae /opt/lampp/htdocs/init-bdd.sh
