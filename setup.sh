7c82ab4ef48aa519468f317b4aa2ea019b8175ac
_N3m0 <n3m0o7@hotmail.com>
2024-04-04 15:41:08 +0200

    deployement: add hotspot + iptable config

--- setup.sh
+++ setup.sh
@@ -17,3 +17,25 @@ docker exec sae chmod +x /opt/lampp/htdocs/init-bdd.sh
 sleep 10
 
 docker exec sae /opt/lampp/htdocs/init-bdd.sh
+
+IFNAME=$(ip link show | grep " UP" | head -n 1 | cut -d ' ' -f 2 | cut -d ':' -f 1)
+CON_NAME="myhotspot"
+
+nmcli con add type wifi ifname $IFNAME con-name $CON_NAME autoconnect yes ssid $CON_NAME
+nmcli con modify $CON_NAME 802-11-wireless.mode ap 802-11-wireless.band bg ipv4.method shared
+
+# nmcli con modify $CON_NAME wifi-sec.key-mgmt wpa-psk
+# nmcli con modify $CON_NAME wifi-sec.psk "MyStrongHotspotPass"
+
+nmcli con up $CON_NAME
+
+iptables -A INPUT -p tcp -m tcp -m multiport --dports 22,80,443 -j ACCEPT
+iptables -A INPUT -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -m state --state ESTABLISHED -j ACCEPT
+iptables -A FORWARD -j DROP
+
+mkdir -p /etc/iptables/
+iptables-save > /etc/iptables/rules.v4
+ip6tables-save > /etc/iptables/rules.v6
+
+apt install iptables-persistent -y
