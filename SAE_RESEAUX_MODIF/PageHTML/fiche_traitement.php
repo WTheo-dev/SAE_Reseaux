1d93e5e56b83d816a98fe080d0bae50060f01d68
Theo <wazydrag.theo@gmail.com>
2024-04-18 17:53:00 +0200

    regle bug

--- SAE_RESEAUX_MODIF/PageHTML/fiche_traitement.php
+++ SAE_RESEAUX_MODIF/PageHTML/fiche_traitement.php
@@ -3,40 +3,51 @@
 include_once '../../APIFinale/fonctions.php';
 define('FICHE_LOCATION', 'location: fiche');
 
+// Vérification et définition des valeurs des clés du tableau $_REQUEST
+$nomDemandeur = isset($_REQUEST['nomDemandeur']) ? $_REQUEST['nomDemandeur'] : '';
+$dateDemande = isset($_REQUEST['dateDemande']) ? $_REQUEST['dateDemande'] : '';
+$localisation = isset($_REQUEST['localisation']) ? $_REQUEST['localisation'] : '';
+$descDemande = isset($_REQUEST['descDemande']) ? $_REQUEST['descDemande'] : '';
+$degreeUrgence = isset($_REQUEST['degreeUrgence']) ? $_REQUEST['degreeUrgence'] : '';
+
+// Création de la fiche
 $id_fiche = creationFiche(
     69,
-    $_REQUEST['nomDemandeur'],
-    $_REQUEST['dateDemande'],
+    $nomDemandeur,
+    $dateDemande,
     date("Y-m-d"),
     "",
-    $_REQUEST['localisation'],
-    $_REQUEST['descDemande'],
-    $_REQUEST['degreeUrgence'],
+    $localisation,
+    $descDemande,
+    $degreeUrgence,
     "", "", "", "", "", "",
     date("Y-m-d"),
-    intval($_POST['id_app']),
+    isset($_POST['id_app']) ? intval($_POST['id_app']) : 0,
     69
 );
+
+// Définition du cookie 'id_fiche'
 setcookie("id_fiche", $id_fiche, -1);
 
+// Gestion des redirections en fonction des actions
 if (isset($_POST['precedent'])) {
     $numpage = $_POST['precedent'];
     $index = $numpage - 1;
     $direction = "precedent";
-    header(FICHE_LOCATION . $index.".php");
+    header(FICHE_LOCATION . $index . ".php");
 } elseif (isset($_POST['sauvegarder'])) {
     if ($_POST['sauvegarder'] == "total") {
         header("location: fiche_total.php");
-    }else {
+    } else {
         $numpage = $_POST['sauvegarder'];
         $index = $numpage;
         $direction = "sauvegarder";
-        header(FICHE_LOCATION . $_POST['sauvegarder'].".php");
+        header(FICHE_LOCATION . $_POST['sauvegarder'] . ".php");
     }
 } elseif (isset($_POST['quitter'])) {
     if ($_POST['quitter'] == "total") {
         header("location: fiche_total.php");
-    }else {
+    } else {
         $numpage = $_POST['quitter'];
         $index = $numpage;
         $direction = "quitter";
@@ -46,23 +57,22 @@ if (isset($_POST['precedent'])) {
     $numpage = $_POST['suivant'];
     $index = $numpage + 1;
     $direction = "suivant";
-    header(FICHE_LOCATION . $index.".php");
+    header(FICHE_LOCATION . $index . ".php");
 } elseif (isset($_POST['from-fiche-valeur'])) {
     header("location: fiche_valeur.php");
 }
 
+// Boucle pour traiter les données POST et définir les cookies correspondants
 foreach ($_POST as $param => $value) {
     $value = htmlspecialchars($value);
     setcookie($param, $value, time() + (86400 * 60));
-    if ((str_starts_with($param, "Texte") || str_starts_with($param, "Icon") || str_starts_with($param, "Audio"))
-    && !isset($_POST[$param])) {
+    if ((str_starts_with($param, "Texte") || str_starts_with($param, "Icon") || str_starts_with($param, "Audio")) && !isset($_POST[$param])) {
         unset($_COOKIE[$param]);
         setcookie($param, '', -1);
     }
 }
 
-
-
+// Fonction pour désélectionner certaines cases à cocher en fonction de la page actuelle
 function uncheckCheckbox($param, $pagenum)
 {
     global $numpage;
@@ -72,6 +82,7 @@ function uncheckCheckbox($param, $pagenum)
     }
 }
 
+// Appel de la fonction uncheckCheckbox pour désélectionner certaines cases à cocher
 uncheckCheckbox("Améliorative", 4);
 uncheckCheckbox("Préventive", 4);
 uncheckCheckbox("Corrective", 4);
@@ -79,3 +90,5 @@ uncheckCheckbox("Aménagement", 5);
 uncheckCheckbox("Finitions", 5);
 uncheckCheckbox("Installation_sanitaire", 5);
 uncheckCheckbox("Nécessite_un_nouvelle_intervention", 7);
+
+?>
